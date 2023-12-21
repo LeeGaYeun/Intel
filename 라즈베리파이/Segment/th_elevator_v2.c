c7c8410938cfffc71e64349db41c6073f5eb6ccd
LeeGaYeun <149138767+LeeGaYeun@users.noreply.github.com>
2023-12-21 16:31:45 +0900

    Create th_elevator_v2.c
    엘리베이터 코드에 쓰레드 적용시킨 코드이다.

---
+++ 라즈베리파이/Segment/th_elevator_v2.c
@@ -0,0 +1,284 @@
+#include <stdio.h>
+#include <string.h>
+#include <wiringPi.h>
+#include <pthread.h>
+
+#define LEDE	0 	//1
+#define LEDD	5 	//2
+#define LEDDP	6 	//3
+#define LEDC	13 //4
+#define LEDG	19 //5
+#define LEDF	15 //7
+#define LEDA	14 //10
+#define LEDB	18 //11
+#define SFT1	12	//12
+#define SFT2	16 //9
+#define SFT3	20	//8
+#define SFT4	21	//6
+
+int CNUM[] = {17, 4, 3, 2};
+int RNUM[] = {25, 7, 1, 24};
+char CT[4][4] = {
+	{'1', '2', '3', 's'},
+	{'4', '5', '6', 'x'},
+	{'7', '8', '9', 'x'},
+	{'*', '0', 'x', 'x'}
+};
+int pattern[][8] = {
+    {1, 1, 1, 1, 1, 1, 0, 0}, // 0
+    {0, 1, 1, 0, 0, 0, 0, 0}, // 1
+    {1, 1, 0, 1, 1, 0, 1, 0}, // 2
+    {1, 1, 1, 1, 0, 0, 1, 0}, // 3
+    {0, 1, 1, 0, 0, 1, 1, 0}, // 4
+    {1, 0, 1, 1, 0, 1, 1, 0}, // 5
+    {1, 0, 1, 1, 1, 1, 1, 0}, // 6
+    {1, 1, 1, 0, 0, 1, 0, 0}, // 7
+    {1, 1, 1, 1, 1, 1, 1, 0}, // 8
+    {1, 1, 1, 0, 0, 1, 1, 0}, // 9
+};
+
+char str[5];
+static int nowf = 1;
+
+void setup();
+char keypad();
+void moveloop(int tnum);
+void currentloop(int tnum);
+void floornum(char key);
+
+pthread_mutex_t lock; // 뮤텍스 선언
+
+void* keypad_thread(void* arg) {
+    while (1) {
+        char key = keypad();
+        if (key != '\0') {
+            pthread_mutex_lock(&lock); // 뮤텍스 락
+            floornum(key);
+            pthread_mutex_unlock(&lock); // 뮤텍스 언락
+        }
+    }
+    return NULL;
+}
+void* display_thread(void* arg) {
+    while (1) {
+        pthread_mutex_lock(&lock); // 뮤텍스 락
+        currentloop(nowf);
+        pthread_mutex_unlock(&lock); // 뮤텍스 언락
+        delay(1); // 원하는 딜레이 값으로 설정
+    }
+    return NULL;
+}
+
+int main() {
+    setup();
+
+    pthread_t keypad_tid, display_tid;
+
+    // 뮤텍스 초기화
+    if (pthread_mutex_init(&lock, NULL) != 0) {
+        printf("Mutex initialization failed.\n");
+        return 1;
+    }
+
+    // 키 입력 스레드 생성
+    if (pthread_create(&keypad_tid, NULL, keypad_thread, NULL) != 0) {
+        printf("Failed to create keypad thread.\n");
+        return 1;
+    }
+
+    // 세그먼트 출력 스레드 생성
+    if (pthread_create(&display_tid, NULL, display_thread, NULL) != 0) {
+        printf("Failed to create display thread.\n");
+        return 1;
+    }
+
+    // 메인 스레드에서 대기
+    pthread_join(keypad_tid, NULL);
+    pthread_join(display_tid, NULL);
+
+    // 뮤텍스 제거
+    pthread_mutex_destroy(&lock);
+
+    return 0;
+}
+
+void setup()
+{
+	wiringPiSetupGpio();
+   static int k = 0;
+   int ledPin[] = {LEDA, LEDB, LEDC, LEDD, LEDE, LEDF, LEDG, LEDDP};
+   int sft[] = {SFT1, SFT2, SFT3, SFT4};
+
+   for(int i = 0; i < 8; i++){
+      pinMode(ledPin[i], OUTPUT);
+   }
+
+   for(int j = 0; j < 4; j++){
+      pinMode(sft[j], OUTPUT);
+      pinMode(RNUM[j], OUTPUT);
+      pinMode(CNUM[j], INPUT);
+      pullUpDnControl(CNUM[j], PUD_UP);
+      digitalWrite(RNUM[j], 1);
+   }
+
+}
+
+char keypad()
+{
+	for(int i = 0; i < 4; i++){
+        //현재 행이 눌렸을 때
+		digitalWrite(RNUM[i], LOW);
+		if(digitalRead(RNUM[i]) == LOW){
+			for(int j = 0; j < 4; j++){
+				if(digitalRead(CNUM[j]) == LOW){
+						delay(200);
+						return CT[i][j];
+				}
+			}
+		}
+		digitalWrite(RNUM[i], HIGH); //다시 모든 행을 HIGH로 설정
+	}
+	return '\0';
+}
+
+void moveloop(int tnum)
+{
+	int digit[3];
+	int k = 0;
+	digit[0] = tnum % 10;
+	digit[1] = (tnum / 10) % 10;
+	digit[2] = (tnum / 100) % 10;
+	digit[3] = (tnum / 1000) % 10;
+	int ledPin[] = {LEDA, LEDB, LEDC, LEDD, LEDE, LEDF, LEDG, LEDDP};
+	int sft[] = {SFT1, SFT2, SFT3, SFT4};
+
+	for(int i = 0; i < 4; i++){
+   	digitalWrite(sft[i], 1);
+   }
+
+	for(k = 0; k < 10; k++){
+   //일의 자리
+   	digitalWrite(sft[3], 0);
+   	for(int j = 0; j < 8; j++){
+      	digitalWrite(ledPin[j], pattern[digit[0]][j]);
+   	}
+   	delay(5);
+   	digitalWrite(sft[3], 1);
+
+	//십의 자리
+   	digitalWrite(sft[2], 0);
+   	for(int j = 0; j < 8; j++){
+			digitalWrite(ledPin[j], pattern[digit[1]][j]);
+   	}
+   	delay(5);
+   	digitalWrite(sft[2], 1);
+
+	//백의 자리
+		digitalWrite(sft[1], 0);
+   	for(int j = 0; j < 8; j++){
+			digitalWrite(ledPin[j], pattern[digit[2]][j]);
+   	}
+   	delay(5);
+   	digitalWrite(sft[1], 1);
+
+   //천의 자리
+      digitalWrite(sft[0], 0);
+   	for(int j = 0; j < 8; j++){
+   		digitalWrite(ledPin[j], pattern[digit[3]][j]);
+   	}
+   	delay(5);
+   	digitalWrite(sft[0], 1);
+   }
+}
+
+void floornum(char key)
+{
+	static int j = 0;
+	static int l = 0;
+	if(key >= '0' && key <= '9'){
+			strncat(str, &key, 1);
+			printf("str : %s\n", str);
+	}
+	else if(key == '*'){
+		str[0] = '\0';
+	}
+	else if(key == 's'){
+		int tnum = atoi(str);
+
+		if(tnum > nowf && tnum < 10000){
+			printf("Going up to %d\n", tnum);
+			int upf = tnum - nowf;
+			for(int i = 1; i <= upf; i++){
+            printf("%d\n", nowf + i);
+            moveloop(nowf + i);
+         }
+         printf("%d 층에 도착하였습니다.\n", tnum);
+			nowf = tnum;
+			str[0] = '\0';
+			currentloop(tnum);
+		}
+		else if(tnum < nowf && tnum < 10000){
+			printf("Going down to %d\n", tnum);
+			int downf = nowf - tnum;
+			for(int i = 1; i <= downf; i++){
+				printf("%d\n", nowf - i);
+				moveloop(nowf - i);
+			}
+			printf("%d 층에 도착하였습니다.\n", tnum);
+			nowf = tnum;
+			str[0] = '\0';
+		}
+		else{
+			printf("층을 9999층 이내로 누르세요.\n");
+			str[0] = '\0';
+		}
+	}
+}
+
+void currentloop(int tnum)
+{
+	int digit[3];
+	int k = 0;
+	digit[0] = tnum % 10;
+	digit[1] = (tnum / 10) % 10;
+	digit[2] = (tnum / 100) % 10;
+	digit[3] = (tnum / 1000) % 10;
+	int ledPin[] = {LEDA, LEDB, LEDC, LEDD, LEDE, LEDF, LEDG, LEDDP};
+	int sft[] = {SFT1, SFT2, SFT3, SFT4};
+
+	for(int i = 0; i < 4; i++){
+   	digitalWrite(sft[i], 1);
+   }
+
+   //일의 자리
+   	digitalWrite(sft[3], 0);
+   	for(int j = 0; j < 8; j++){
+      	digitalWrite(ledPin[j], pattern[digit[0]][j]);
+   	}
+   	delay(5);
+   	digitalWrite(sft[3], 1);
+
+	//십의 자리
+   	digitalWrite(sft[2], 0);
+   	for(int j = 0; j < 8; j++){
+			digitalWrite(ledPin[j], pattern[digit[1]][j]);
+   	}
+   	delay(5);
+   	digitalWrite(sft[2], 1);
+
+	//백의 자리
+		digitalWrite(sft[1], 0);
+   	for(int j = 0; j < 8; j++){
+			digitalWrite(ledPin[j], pattern[digit[2]][j]);
+   	}
+   	delay(5);
+   	digitalWrite(sft[1], 1);
+
+   //천의 자리
+      digitalWrite(sft[0], 0);
+   	for(int j = 0; j < 8; j++){
+   		digitalWrite(ledPin[j], pattern[digit[3]][j]);
+   	}
+   	delay(5);
+   	digitalWrite(sft[0], 1);
+}
