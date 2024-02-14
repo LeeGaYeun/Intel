e3367804e3a6cf0978e355aca78d7f1daeb434b1
EMBEDDED <lgyeunb777@naver.com>
2024-02-14 12:01:12 +0900

    [2024.02.14]OpenCV_7일차

--- CH_7/noise/HelloCV.cpp
+++ CH_7/noise/HelloCV.cpp
@@ -6,11 +6,13 @@ using namespace std;
 
 void noise_gaussian();
 void bilateral();
+void median();
 
 int main()
 {
     //noise_gaussian();
-    bilateral();
+    //bilateral();
+    median();
     return 0;
 }
 
@@ -53,6 +55,34 @@ void bilateral()
     Mat dst2;
     bilateralFilter(src, dst2, -1, 10, 5);
     imshow("dst2", dst2);
+
+    waitKey();
+    destroyAllWindows();
+}
+
+void median()
+{
+    Mat src = imread("../../images/lena.jpg", IMREAD_GRAYSCALE);
+
+
+    int num = (int)(src.total() * 0.1);
+    for (size_t i = 0; i < num; i++)
+    {
+        int x = rand() % src.cols;
+        int y = rand() % src.rows;
+        src.at<uchar>(y, x) = (i % 2) * 255;
+    }
+    imshow("image", src);
+
+
+    Mat dst1;
+    GaussianBlur(src, dst1, Size(), 1);
+    imshow("gaussian", dst1);
+
+    Mat dst2;
+    medianBlur(src, dst2, 3);
+    imshow("median", dst2);
+
 
     waitKey();
     destroyAllWindows();
