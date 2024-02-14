4b95be5012c60501e8a586d982ca74cfefc03e55
EMBEDDED <lgyeunb777@naver.com>
2024-02-14 16:58:17 +0900

    [2024.02.14]OpenCV_7일차

---
+++ CH_11/CH_11/HelloCV.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+void erode_dilate();
+void open_close();
+
+int main()
+{
+    //erode_dilate();
+    open_close();
+    return 0;
+}
+
+void erode_dilate()
+{
+    Mat src = imread("../../images/baboon.jpg", IMREAD_GRAYSCALE);
+
+    if (src.empty())
+    {
+        cerr << "Image load failed" << endl;
+        return;
+    }
+
+    Mat bin;
+    threshold(src, bin, 0, 255, THRESH_BINARY | THRESH_OTSU);
+
+    Mat dst1, dst2;
+    erode(bin, dst1, Mat());
+    dilate(bin, dst2, Mat());
+
+    imshow("src", src);
+    imshow("bin", bin);
+    imshow("erode", dst1);
+    imshow("dilate", dst2);
+
+    waitKey();
+    destroyAllWindows();
+}
+
+void open_close()
+{
+    Mat src = imread("../../images/baboon.jpg", IMREAD_GRAYSCALE);
+
+    if (src.empty())
+    {
+        cerr << "Image load failed" << endl;
+        return;
+    }
+
+    Mat bin;
+    threshold(src, bin, 0, 255, THRESH_BINARY | THRESH_OTSU);
+
+    Mat dst1, dst2;
+    morphologyEx(bin, dst1, MORPH_OPEN, Mat());
+    morphologyEx(bin, dst2, MORPH_CLOSE, Mat());
+
+    imshow("src", src);
+    imshow("bin", bin);
+    imshow("opening", dst1);
+    imshow("closing", dst2);
+
+    waitKey();
+    destroyAllWindows();
+}
