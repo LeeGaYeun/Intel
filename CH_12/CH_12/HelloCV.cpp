921b14691b79861f24dc2a2426737a7da1f3aa6d
EMBEDDED <lgyeunb777@naver.com>
2024-02-16 16:44:55 +0900

    [2024.02.16]OpenCV_9일차

---
+++ CH_12/CH_12/HelloCV.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+void labeling_basic();
+void labeling_basic2();
+
+int main()
+{
+	//labeling_basic();
+	labeling_basic2();
+	system("pause");
+    
+    return 0;
+}
+
+void labeling_basic()
+{
+	uchar data[] = {
+		0, 0, 1, 1, 0, 0, 0, 0,
+		1, 1, 1, 1, 0, 0, 1, 0,
+		1, 1, 1, 1, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 1, 1, 0,
+		0, 0, 0, 1, 1, 1, 1, 0,
+		0, 0, 0, 1, 0, 0, 1, 0,
+		0, 0, 1, 1, 1, 1, 1, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+	};
+
+	Mat src = Mat(8, 8, CV_8UC1, data) * 255;
+
+	Mat labels;
+	int cnt = connectedComponents(src, labels);
+
+	cout << "src:\n" << src << endl;
+	cout << "labels:\n" << labels << endl;
+	cout << "number of labels: " << cnt << endl;
+}
+
+void labeling_basic2()
+{
+	uchar data[] = {
+		0, 0, 1, 1, 0, 0, 0, 0,
+		1, 1, 1, 1, 0, 0, 1, 0,
+		1, 1, 1, 1, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 1, 1, 0,
+		0, 0, 0, 1, 1, 1, 1, 0,
+		0, 0, 0, 1, 0, 0, 1, 0,
+		0, 0, 1, 1, 1, 1, 1, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+	};
+
+	Mat src = Mat(8, 8, CV_8UC1, data) * 255;
+
+	Mat labels;
+
+	Mat stats, centroids;
+
+	int cnt = connectedComponentsWithStats(src, labels, stats, centroids);
+
+	Mat dst;
+	cvtColor(src, dst, COLOR_GRAY2BGR);
+
+	for (int i = 1; i < cnt; i++)
+	{
+		int* p = stats.ptr<int>(i);
+		//if (p[4] < 20) continue;
+		rectangle(dst, Rect(p[0], p[1], p[2], p[3]), Scalar(0, 255, 255));
+	}
+
+	cout << "src:\n" << src << endl;
+	cout << "labels:\n" << labels << endl;
+	cout << "number of labels: " << cnt << endl;
+}
