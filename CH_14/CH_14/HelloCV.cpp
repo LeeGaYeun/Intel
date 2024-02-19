1430944c546b0c29e9cf053d705159b9d1c6b3da
EMBEDDED <lgyeunb777@naver.com>
2024-02-19 13:53:11 +0900

    [2024.02.19]OpenCV_10일차

---
+++ CH_14/CH_14/HelloCV.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+void corner_harris();
+
+int main()
+{
+	corner_harris();
+
+    return 0;
+}
+
+void corner_harris()
+{
+	Mat src = imread("../../images/house.jpg", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "Image load failed!" << endl;
+		return;
+	}
+
+	Mat harris;
+	cornerHarris(src, harris, 3, 3, 0.04);
+
+	Mat harris_norm;
+	normalize(harris, harris_norm, 0, 255, NORM_MINMAX, CV_8U);
+
+	Mat dst;
+	cvtColor(src, dst, COLOR_GRAY2BGR);
+
+	for (int j = 1; j < harris.rows - 1; j++) {
+		for (int i = 1; i < harris.cols - 1; i++) {
+			if (harris_norm.at<uchar>(j, i) > 120) {
+				if (harris.at<float>(j, i) > harris.at<float>(j - 1, i) &&
+					harris.at<float>(j, i) > harris.at<float>(j + 1, i) &&
+					harris.at<float>(j, i) > harris.at<float>(j, i - 1) &&
+					harris.at<float>(j, i) > harris.at<float>(j, i + 1)) {
+					circle(dst, Point(i, j), 5, Scalar(0, 0, 255), 2);
+				}
+			}
+		}
+	}
+
+	imshow("src", src);
+	imshow("harris_norm", harris_norm);
+	imshow("dst", dst);
+
+	waitKey();
+	destroyAllWindows();
+}
---
+++ CH_14/FAST/HelloCV.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+void corner_fast();
+
+int main()
+{
+	corner_fast();
+
+    return 0;
+}
+
+void corner_fast()
+{
+	Mat src = imread("../../images/house.jpg", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "Image load failed!" << endl;
+		return;
+	}
+
+	vector<KeyPoint> keypoints;
+	FAST(src, keypoints, 60, true);
+
+	Mat dst;
+	cvtColor(src, dst, COLOR_GRAY2BGR);
+
+	for (KeyPoint kp : keypoints) {
+		Point pt(cvRound(kp.pt.x), cvRound(kp.pt.y));
+		circle(dst, pt, 5, Scalar(0, 0, 255), 2);
+	}
+
+	imshow("src", src);
+	imshow("dst", dst);
+
+	waitKey();
+	destroyAllWindows();
+}
